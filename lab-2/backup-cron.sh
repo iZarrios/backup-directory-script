5fc6641a2e02792a1fa012a71a75e5d627873450
iZarrios <zarriose@gmail.com>
2022-10-17 12:13:39 +0200

    updated backup-cron.sh

--- lab-2/backup-cron.sh
+++ lab-2/backup-cron.sh
@@ -1,59 +1,62 @@
 #! /bin/bash
 
+# helper function incase of faulty args
+
 error_args(){
     echo "./backupd.sh DIR BACKUPDIR INTERVAL-SECS MAX-BACKUPS"
     exit 1
 }
 
-re="^[1-9]+$"
+# regular expression for numerical validation
+re="^[0-9]+$" # keep searching for 1-9 values until false
 
-if [[ $# < 4 ]]
+if [[ $# < 4 ]] 
 then
+    # if wrong number of args
     error_args
 else
 
-    # dir=$(realpath $1)
-    # backupdir=$(realpath $2)
-
-    # echo $dir
-    # echo $backupdir
-    # exit 0
     if [[ $1 == "." ]]
     then
         dir=$(pwd)
     else
-        dir=$1
+        dir=$1 # or we can use $(relapath($1))
     fi
 
     if [[ $2 == "." ]]
     then
         error_args
     else
-        backupdir=$2
+        backupdir=$2 # or we can use $(relapath($2))
     fi
 
     if ! [[ $3 =~ $re ]] ; then
         echo "error: Not a number" 
         error_args
     else
-        interval=$3
+        interval=$3 # what if the value is 0? sleep 0
     fi
 
-    if ! [[ $3 =~ $re ]] ; then
+    if ! [[ $4 =~ $re ]] ; then
         echo "error: Not a number" 
         error_args
     else
-        max_backups=$4
+        if [[ $4 == 0 ]]; then # what if the value is 0
+            echo "max_backups cannot be less than 1"
+            exit 1
+        fi
+        max_backups=$4 
     fi
 fi
 
-# if [[ -d $backupdir ]] 
-# then
-#     echo "$(realpath $backupdir)...already exists"
-#     exit 1
-# else
-#     mkdir $backupdir
-# fi
+if [[ -d $backupdir ]] 
+then
+    echo "$(realpath $backupdir)...already exists"
+    echo "Care that maybe another directory is already backed up in $backupdir"
+else
+    echo "Created $backupdir"
+    mkdir $backupdir
+fi
 
 cd $backupdir
 
@@ -81,7 +84,7 @@ do
     
     # TODO maybe change it to a while loop
     if [[ $count > $max_backups ]]; then
-        file_delete=$(ls  | sort | head -1)
+        file_delete=$(ls  | sort | head -1) # no need for piping to sort program as ls/ ls -r already outputs sorted manner.
         count=`expr $count - 1`
         echo "Deleted...$file_delete"
         rm -r $file_delete
